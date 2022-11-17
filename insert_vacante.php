0fd496b86a25bd72ff82361ce16c6f8683b1a454
oliverjx <115738356+oliverjx@users.noreply.github.com>
2022-11-17 18:39:52 -0400

    se agrego conecion personal(oliverjx) y formulario de vacantes

---
+++ insert_vacante.php
@@ -0,0 +1,23 @@
+<?php
+         include_once 'conexion_o.php';
+
+                  $empresa = $_REQUEST['empr'];
+                  $puesto = $_REQUEST['nomb'];
+                  $funcion = $_REQUEST['func'];
+                  $sueldo = $_REQUEST['suel'];
+                  $contrato = $_REQUEST['cont'];
+                  $correo = $_REQUEST['corr'];
+                  $persona = $_REQUEST['pers'];
+                  $telefono = $_REQUEST['telf'];
+                  $hora_a = $_REQUEST['hora_a'];
+                  $hora_b = $_REQUEST['hora_b'];
+                  
+                
+$consulta =  "INSERT INTO `ipisa_pag`.`vacantes` (`id_empresa`, `nom_puesto`, `funcion_puesto`, `sueldo`, `tipo_contrato`, `correo_curriculum`, `persona_contacto`, `telefono`, `hora_entrada`, `hora_salida`) VALUES ('$empresa', '$puesto', '$funcion', '$sueldo', '$contrato', '$correo', '$persona', '$telefono', '$hora_a', '$hora_b');";
+                echo $consulta;
+                mysqli_query($mysqli, $consulta);
+               
+                
+
+            //header("location: vacantes.php?insert=success");
+            ?>
--- vacantes.php
+++ vacantes.php
@@ -4,16 +4,13 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Vacantes - formulario</title>
-
+    <title>alumno</title>
     <link rel="icon" href="imagenes/logo-1.png">
-
     <link rel="stylesheet" href="menu.css">
     <link rel="stylesheet" href="css.css">
     <link rel="stylesheet" href="footer.css">
     <link rel="stylesheet" href="contenido.css">
 </head>
-<body>
 <body>
 <header>
             
@@ -73,59 +70,121 @@
             
             
             </nav>
-        </header>    
+        </header> 
 
-    <footer>
+<br> <br>
 
-        <div class="container">	
-            <div class="sec aboutus">
-                <h2>Sobre nosotros</h2>
-                        <p>Somos una institución educativa técnico profesional con uno de los más altos niveles 
-                        de expansión y crecimiento en este tipo de modelo de educación, así como también, acorde 
-                        a la vanguardia de los nuevos tiempos.</p>
-                <ul class="redes">
-                 <li><a href="https://www.facebook.com/Bibliotecamp/"><i class="fab fa-facebook-square"></i></a></li>
-                 <li><a href="https://www.instagram.com/biblioteca_mp/"><i class="fab fa-instagram-square"></i></a></li>
-                 <link rel="stylesheet" type="text/css" href="css/css-footer.css">
-                </ul>
-        </div>
-          
-        <div class="sec quickLinks">
-            <h2>Enlaces rápidos</h2>
-               <ul>
-                 <li><a href="index.html">Inicio</a></li>
-                 <li><a href="pasantia.html">Pasantía</a></li>
-                 <li><a href="colaboradores.html">Colaboradores</a></li>
-                 <li><a href="familia.html">Familia</a></li>
-                 <li><a href="alumno.html">Alumno</a></li>
-               </ul>
-        </div>
-          
-        <div class= "sec contact">
-            <h2>Contacto</h2>
-               <ul class="info">
-                 <li>
-                 <span><i class="fas fa-map-marker-alt"></i></span>
-                 <span>Av. Hispanoamericana, Km 1 <br>
-                  Santiago,<br>República Dominicana</span>
-                 </li>
-                <li>
-                 <span><i class="fas fa-phone-alt"></i></span>
-                 <p><a href="tel:8293762176">+1 809 724 5700</a><br></p>
-                </li>
-                <li>
-                 <span></span>
-                 <p><a href="https://www.instagram.com/ipisasdb/?igshid=YmMyMTA2M2Y%3D">Instagram</a><br></p>
-                </li>
-                <li>
-                 <span></span>
-                 <p><a href="https://www.facebook.com/IPISASDB">Facebook</a><br></p>
-                </li>          
-        </div>
-                
-    </footer>
+<center>
+
+        <form action="insert_vacante.php" method="post" >
+            <label >Empresa: </label>
+            <input type="text" name="empr"><br> <br>
+            
+            <label >Nombre del puesto: </label>
+            <input type="text" name="nomb"><br> <br>
+            
+            <label >Funcion del puesto: </label>
+            <input type="text" name="func"><br> <br>
+            
+            <label >sueldo: </label>
+            <input type="text" name="suel" ><br> <br>
+
+            <label >contrato: </label>
+            <select type="text" name="cont">
+                <option value="0">fijo</option>
+                <option value="1">temporal</option>
+            </select> <br> <br>
+
+            <label >Correo curriculum: </label>
+            <input type="text" name="corr"><br> <br>
+
+            <label > persona contacto: </label>
+            <input type="text" name="pers"><br> <br>
+
+            <label >telefono: </label>
+            <input type="text" name="telf"><br> <br>
+
+            <label >horario: </label>
+                <label >hora de entrada: 
+                    <select name="hora_a" >
+                        <option value="8:00:00">8:00</option>
+                        <option value="9:00:00">9:00</option>
+                        <option value="10:00:00">10:00</option>
+                        <option value="11:00:00">11:00</option>
+                        <option value="12:00:00">12:00</option>
+                        <option value="13:00:00">13:00</option>
+                    </select>
+                </label>
+
+           
+                <label >hora de salida: 
+                    <select name="hora_b" >
+                        <option value="14:00:00">14:00</option>
+                        <option value="15:00:00">15:00</option>
+                        <option value="16:00:00">16:00</option>
+                        <option value="17:00:00">17:00</option>
+                        <option value="18:00:00">18:00</option>
+                        <option value="19:00:00">19:00</option>
+                    </select>
+                </label>
 
 
+            <br> <br>
+
+            <button>enviar</button>
+        
+        </form>
+        </center>
+
+            <footer>
+
+                <div class="container">	
+                    <div class="sec aboutus">
+                        <h2>Sobre nosotros</h2>
+                                <p>Somos una institución educativa técnico profesional con uno de los más altos niveles 
+                                de expansión y crecimiento en este tipo de modelo de educación, así como también, acorde 
+                                a la vanguardia de los nuevos tiempos.</p>
+                        <ul class="redes">
+                         <li><a href="https://www.facebook.com/Bibliotecamp/"><i class="fab fa-facebook-square"></i></a></li>
+                         <li><a href="https://www.instagram.com/biblioteca_mp/"><i class="fab fa-instagram-square"></i></a></li>
+                         <link rel="stylesheet" type="text/css" href="css/css-footer.css">
+                        </ul>
+                </div>
+                  
+                <div class="sec quickLinks">
+                    <h2>Enlaces rápidos</h2>
+                       <ul>
+                         <li><a href="index.html">Inicio</a></li>
+                         <li><a href="pasantia.html">Pasantía</a></li>
+                         <li><a href="colaboradores.html">Colaboradores</a></li>
+                         <li><a href="familia.html">Familia</a></li>
+                         <li><a href="alumno.html">Alumno</a></li>
+                       </ul>
+                </div>
+                  
+                <div class= "sec contact">
+                    <h2>Contacto</h2>
+                       <ul class="info">
+                         <li>
+                         <span><i class="fas fa-map-marker-alt"></i></span>
+                         <span>Av. Hispanoamericana, Km 1 <br>
+                          Santiago,<br>República Dominicana</span>
+                         </li>
+                        <li>
+                         <span><i class="fas fa-phone-alt"></i></span>
+                         <p><a href="tel:8293762176">+1 809 724 5700</a><br></p>
+                        </li>
+                        <li>
+                         <span></span>
+                         <p><a href="https://www.instagram.com/ipisasdb/?igshid=YmMyMTA2M2Y%3D">Instagram</a><br></p>
+                        </li>
+                        <li>
+                         <span></span>
+                         <p><a href="https://www.facebook.com/IPISASDB">Facebook</a><br></p>
+                        </li>        
+                </div>
+            </div>
+            </footer>
 
 </body>
 </html>
